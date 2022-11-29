fcaa318b4da09bd0461be24d11e899979af8d747
RafaelPires2 <rafaelbrpires@gmail.com>
2022-11-29 11:54:13 -0300

    validacoes e requisicao

--- main.js
+++ main.js
@@ -4,38 +4,53 @@ const cep = document.getElementById('cep');
 
 const renderResults = document.getElementById('renderResults');
 
+const mensagemErro = 'Digite um CEP válido exemplo: "01153000"' 
+
+
+
 function getData(){
-    cep.addEventListener('input', () =>{
-        if(cep.value.length > 8){
-          cep.value = cep.value.substring(0, 8);
-        }
-    } )
+  cep.addEventListener('input', () =>{
+    if(cep.value.length > 8){  
+      cep.value = cep.value.substring(0, 8);
+    }
+  })
 
-    botao.addEventListener("click",() => {
-        const url = `https://viacep.com.br/ws/${cep.value}/json/`
+  botao.addEventListener("click",() => {
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`
+    
+    if(cep.length == 8){
+      renderResults.innerHTML = `${mensagemErro}` 
+      renderResults.style.color = 'red'
+      cep.value = ""          
+   } 
+
+    axios.get(url)
+    .then(res => {
+      const data = res.data 
+      
+      if(data.erro === true){
         
-        if(cep.length !== 8){
-            renderResults.innerHTML = 'Digite um CEP válido' 
-            renderResults.style.color = 'red'
-            cep.value = ""          
-        }
+        renderResults.innerHTML = `${mensagemErro}` 
+        renderResults.style.color = 'red'
 
-        axios.get(url)
-        .then(res => {
-            const data = res.data
-            
-            renderResults.innerHTML = `
-            Endereço: ${data.logradouro} -
-            Bairro: ${data.bairro} -
-            CEP: ${data.cep} -
-            Cidade: ${data.localidade} -
-            Estado: ${data.uf}
-            `
-            renderResults.style.color = 'green'
-            cep.value = ""
-        })
-        .catch(error => console.log(error))
+      }else{
+          renderResults.innerHTML = `
+          Endereço: ${data.logradouro} -
+          Bairro: ${data.bairro} -
+          CEP: ${data.cep} -
+          Cidade: ${data.localidade} -
+          Estado: ${data.uf}
+          `
+          renderResults.style.color = 'green'
+          cep.value = ""
+
+      }
+
+    })
+      .catch(error => console.log(error))
 })
+    
+
 }
 getData()
 
